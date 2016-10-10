d5d555583ce7b6d31e966d79f2c0b4ed1e11e27a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-10-10 22:51:47 +0200

    add operators and constructors, complete task

--- main.cpp
+++ main.cpp
@@ -11,58 +11,69 @@ void main()
     Zbior<> z2(2);
     Zbior<> z3(3);
     Zbior<> z12356("1101110");
-    //cout << z << endl;       //  { }
-    //cout << z2 << endl;      //  { 2 }
-    //cout << z3 << endl;      //  { 3 }
-    //cout << z12356 << endl;  //  { 1 , 2 , 3 , 5 , 6 }
+    cout << z << endl;       //  { }
+    cout << z2 << endl;      //  { 2 }
+    cout << z3 << endl;      //  { 3 }
+    cout << z12356 << endl;  //  { 1 , 2 , 3 , 5 , 6 }
 
-    //cout << endl << "Suma, iloczyn, roznica" << endl;
-    //Zbior<> z23;
-    //Zbior<> z156;
-    //Zbior<> zz;
-    //z23  = z2+z3;
-    //zz   = z2*z3;
-    //z156 = z12356-z23;
-    //cout << z23 << endl;     //  { 2 , 3 }
-    //cout << zz << endl;      //  { }
-    //cout << z156 << endl;    //  { 1 , 5 , 6 }
+    cout << endl << "Suma, iloczyn, roznica" << endl;
+    Zbior<> z23;
+    Zbior<> z156;
+    Zbior<> zz;
+    z23  = z2+z3;
+    zz   = z2*z3;
+    z156 = z12356-z23;
+    cout << z23 << endl;     //  { 2 , 3 }
+    cout << zz << endl;      //  { }
+    cout << z156 << endl;    //  { 1 , 5 , 6 }
 
-    //cout << endl << "Wstawianie i usuwanie elementu" << endl;
-    //z23+=4;
-    //z156-=6;
-    //cout << z23 << endl;     //  { 2 , 3 , 4 }
-    //cout << z156 << endl;    //  { 1 , 5 }
+    cout << endl << "Wstawianie i usuwanie elementu" << endl;
+    z23+=4;
+    z156-=6;
+    cout << z23 << endl;     //  { 2 , 3 , 4 }
+    cout << z156 << endl;    //  { 1 , 5 }
 
-    //cout << endl << "Zawieranie" << endl;
-    //bool b1= z156<=z12356;
-    //bool b2= z23<=z12356;
-    //cout << b1 << endl;           //  1
-    //cout << b2 << endl;           //  0
-    //cout << (z23>=z) << endl;     //  1
-    //cout << (z23>=z156) << endl;  //  0
+    cout << endl << "Zawieranie" << endl;
+    bool b1= z156<=z12356;
+    bool b2= z23<=z12356;
+    cout << b1 << endl;           //  1
+    cout << b2 << endl;           //  0
+    cout << (z23>=z) << endl;     //  1
+    cout << (z23>=z156) << endl;  //  0
 
-    //cout << endl << "Dopelnienie" << endl;
-    //Zbior<8> d1("1010101");
-    //Zbior<8> d2;
-    //d2=~d1;
-    //cout << d1 << endl;      //  { 0 , 2 , 4 , 6 }
-    //cout << d2 << endl;      //  { 1 , 3 , 5 , 7 }
+    cout << endl << "Dopelnienie" << endl;
+    Zbior<8> d1("1010101");
+    Zbior<8> d2;
+    d2=~d1;
+    cout << d1 << endl;      //  { 0 , 2 , 4 , 6 }
+    cout << d2 << endl;      //  { 1 , 3 , 5 , 7 }
 
-    //cout << endl << "Metody" << endl;
-    //cout << z.empty() << endl;    //  1
-    //cout << z23.empty() << endl;  //  0
-    //cout << z.count() << endl;    //  0
-    //cout << z23.count() << endl;  //  3
+    cout << endl << "Metody" << endl;
+    cout << z.empty() << endl;    //  1
+    cout << z23.empty() << endl;  //  0
+    cout << z.count() << endl;    //  0
+    cout << z23.count() << endl;  //  3
 
-    //cout << endl << "Wyjasnij to" << endl;
-    //Zbior<> x, y;
-    //x=x+1+2+3+4+5+6+7;
-    //y+=1+2+3+4+5+6+7;
-    //cout << x << endl;       // { 1 , 2 , 3 , 4 , 5 , 6 , 7 }
-    //cout << y << endl;       // { 28 }
+    cout << endl << "Wyjasnij to" << endl;
+	// Tutaj pewnie chodzilo o to, ze kazda z liczb 1, 2, ..., 7 w `x`
+	// jest traktowana jako zbior i nastepuje dodawanie zbiorow (suma mnogosciowa)
+	// Natomiast w `y`, jako ze zdefiniowalismy operator+= jako przyjmujacy liczbe (int)
+	// po prawej stronie, to te liczby sa po prostu dodane, a dopiero potem zostaje
+	// wykonana operacja dodania do zbioru liczby 28.
+
+	// Niestety u mnie nie dziala `x` w bazowej wersji (liczby nie sa przerabiane
+	// na obiekty), wiec musialem dorobic oddzielny operator+(zbior, liczba)
+	// Jezeli ktos ma pomysl dlaczego tak sie dzieje to zapraszam do dyskusji
+    Zbior<> x, y;
+    x=x+1+2+3+4+5+6+7;
+    y+=1+2+3+4+5+6+7;
+    cout << x << endl;       // { 1 , 2 , 3 , 4 , 5 , 6 , 7 }
+    cout << y << endl;       // { 28 }
 
 
-	//cout<<"Tu napisz kawalek kodu sprawdzajacy czy liczba 2 nalezy do zbioru z23, ma byc jak najprostszy !!!"<<endl<<((z23.z[3]) ? "Nalezy" : "Nie nalezy")<<endl;
+	cout << "Tu napisz kawalek kodu sprawdzajacy czy liczba 2 nalezy do zbioru z23, ma byc jak najprostszy !!!" << endl;
+	cout << (z23[2] ? "Nalezy" : "Nie nalezy") << endl;
+
 
     cout << endl;
 
--- zbior.h
+++ zbior.h
@@ -1,16 +1,115 @@
 #pragma once
+#include <iostream>
 #include <bitset>
 using namespace std;
 
 template <unsigned N=32>
 class Zbior: public bitset<N> {
 public:
-	Zbior(unsigned liczba = 0);
+	Zbior(int liczba = -1);
 	Zbior(char *zbiorBitow) : bitset(zbiorBitow) { };
+	Zbior(bitset<N> zbiorBitow) : bitset(zbiorBitow) { };
+
+	bool empty() const;
+	unsigned count() const;
 };
 
 template <unsigned N>
-Zbior<N>::Zbior(unsigned liczba) {
+Zbior<N>::Zbior(int liczba) : bitset(0) {
+	if (liczba < 0 || liczba > N - 1) {
+		return;
+	}
+
 	
+	(*this)[liczba] = 1;
 }
 
+
+template <unsigned N>
+ostream& operator<<(ostream &os, Zbior<N>& zb) {
+	if (zb.empty()) {
+		return os << "{ }";
+	}
+
+	// Zbior niepusty
+	bool wstawPrzecinek = false;
+
+	os << "{ ";
+	for (int i = 0; i < N; i++) {
+		if (zb[i]) {
+			if (wstawPrzecinek) {
+				os << ", ";
+			}
+
+			os << i;
+			wstawPrzecinek = true;
+		}
+	}
+
+	return os << " }";
+}
+
+template <unsigned N>
+Zbior<N> operator+(Zbior<N>& z1, Zbior<N>& z2) {
+	return z1 | z2;
+}
+
+template <unsigned N>
+Zbior<N> operator+(Zbior<N> z1, int liczba) {
+	z1[liczba] = true;
+	return z1;
+}
+
+template <unsigned N>
+Zbior<N> operator-(Zbior<N>& z1, Zbior<N> z2) {
+	// Wazne: z2 nie jest referencja, bo uzywamy flip,
+	// ktore zmutowaloby zawartosc z2.
+	// Stad z2 musi byc przeslane jako kopia.
+	// Alternatywnie moznaby tworzyc kopie lokalnie,
+	// ale nie ma potrzeby jak mozna zrobic tak jak tu.
+	return z1 & z2.flip();
+}
+
+template <unsigned N>
+Zbior<N> operator*(Zbior<N>& z1, Zbior<N>& z2) {
+	return z1 & z2;
+}
+
+template <unsigned N>
+Zbior<N>& operator+=(Zbior<N>& z1, int liczba) {
+	z1[liczba] = true;
+	return z1;
+}
+
+template <unsigned N>
+Zbior<N>& operator-=(Zbior<N>& z1, int liczba) {
+	z1[liczba] = false;
+	return z1;
+}
+
+template <unsigned N>
+bool operator<=(Zbior<N>& z1, Zbior<N>& z2) {
+	return (z1 * z2).count() == z1.count();
+}
+
+template <unsigned N>
+bool operator>=(Zbior<N>& z1, Zbior<N>& z2) {
+	return (z1 * z2).count() == z2.count();
+}
+
+template <unsigned N>
+Zbior<N> operator~(Zbior<N> zbior) {
+	// Znowu uzywamy funkcji mutujacej (flip),
+	// wiec musimy dzialac na kopii, a nie referencji do `zbior`
+	return zbior.flip();
+}
+
+template <unsigned N>
+bool Zbior<N>::empty() const {
+	return count() == 0;
+}
+
+template <unsigned N>
+unsigned Zbior<N>::count() const {
+	return static_cast<bitset>(*this).count();
+}
